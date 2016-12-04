cd62321ea772f3e83d9bdcc5f357fd502f168717
Jason Shin <jasonshin98@gmail.com>
2016-12-04 12:01:49 -0500

    basic players made

---
+++ BasketballPlayer.java
@@ -0,0 +1,52 @@
+public class BasketballPlayer extends Player {
+    private int points;
+    private int rebounds;
+    private int assists;
+    private int steals;
+    private int blocks;
+    private int gamesPlayed;
+
+    public BasketballPlayer(String name, int birthday, int number, int weight, int height) {
+	super(name, birthday, number, weight, height);
+	points = 0;
+	rebounds = 0;
+	assists = 0;
+	steals = 0;
+	blocks = 0;
+	gamesPlayed = 0;
+    }
+
+    public int getPoints() {
+	return points;
+    }
+
+    public void setPoints(int points) {
+	this.points = points;
+    }
+
+    public int getRebounds() {
+	return rebounds;
+    }
+
+    public void setRebounds(int rebounds) {
+	this.rebounds = rebounds;
+    }
+
+    public int getAssists() {
+	return assists;
+    }
+
+    public void setSteals(int steals) {
+	this.steals = steals;
+    }
+
+    public int getBlocks() {
+	return blocks;
+    }
+
+    public void setBlocks(int blocks) {
+	this.blocks = blocks;
+    }  
+
+    
+}
---
+++ Player.java
@@ -0,0 +1,55 @@
+public class Player {
+    private String name;
+    private int birthday;
+    private int number;
+    private int weight;
+    private int height;
+
+    public Player(String name, int birthday, int number, int weight, int height) {
+	this.name = name;
+	this.birthday = birthday;
+	this.number = number;
+	this.weight = weight;
+	this.height = height;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public int getBirthday() {
+	return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+	this.birthday = birthday;
+    }
+
+    public int getNumber() {
+	return number;
+    }
+
+    public void setNumber(int number) {
+	this.number = number;
+    }
+    
+    public int getWeight() {
+	return weight;
+    }
+
+    public void setWeight(int weight) {
+	this.weight = weight;
+    }
+
+    public int getHeight() {
+	return height;
+    }
+
+    public void setHeight(int height) {
+	this.height = height;
+    }
+}
